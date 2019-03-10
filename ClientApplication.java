7d07fba5c8705848f282781ef837afaede4f24c6
samsunder3577 <blackops034561@gmail.com>
2019-03-10 11:57:17 +0530

    End to End Morse

---
+++ ClientApplication.java
@@ -0,0 +1,123 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.*; 
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+import java.util.*;
+class ClientApplication extends JFrame {
+    public static void main(String args[]) {
+
+        
+        JFrame frame = new JFrame("ApMSaM");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 100);
+
+        
+        //JMenuBar mb = new JMenuBar();
+       // JMenu m1 = new JMenu("FILE");
+       // JMenu m2 = new JMenu("Help");
+        //mb.add(m1);
+       // mb.add(m2);
+       // JMenuItem m11 = new JMenuItem("Open");
+        //JMenuItem m22 = new JMenuItem("Save as");
+        //m1.add(m11);
+        //m1.add(m22);
+
+        
+        JPanel panel = new JPanel(); 
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(50); 
+        JButton send = new JButton("Send");
+        //JButton reset = new JButton("Reset");
+        panel.add(label); 
+        panel.add(label); 
+        panel.add(tf);
+        panel.add(send);
+        //panel.add(reset);
+
+        
+        //JTextArea ta = new JTextArea();
+
+       
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+       // frame.getContentPane().add(BorderLayout.NORTH, mb);
+        //frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+		 
+	send.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {try
+		  {
+        BufferedWriter bw=new BufferedWriter(new FileWriter("sample.txt"));
+		bw.write(tf.getText());
+		
+		tf.setText("");
+		bw.close();
+		
+		char[] english = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                  'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                  ',', '.', '?' , ' ' ,'-' ,'@'};
+
+		String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
+                ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+                "-----", "--..--", ".-.-.-", "..--..", " " ,"_...._" ,".__._."};
+				
+		FileReader fre = new FileReader("sample.txt");
+		BufferedReader infr=new BufferedReader(fre);
+		String sss=infr.readLine();
+		String userInput = sss.toLowerCase();
+
+		char[] chars = userInput.toCharArray();
+
+		String stre = "";
+		for (int i = 0; i < chars.length; i++)
+		{
+        for (int j = 0; j < english.length; j++)
+		{
+
+            if (english[j] == chars[i])
+			{
+                stre = stre + morse[j] + " ";  
+			}
+		}
+		}
+        
+		FileWriter fwe = new FileWriter("sample.txt", false);
+		fwe.write(stre); 
+		fwe.flush();
+		infr.close();
+		fwe.close();
+		
+		Socket client=new Socket("localhost",6666);
+		FileReader fr = new FileReader("sample.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		DataOutputStream outToServer = new DataOutputStream(client.getOutputStream());
+		//FileWriter fw = new FileWriter("output.txt", true);
+		String s;
+		
+		while ((s = br.readLine()) != null) { 
+				//fw.write(s); 
+				//fw.flush();
+				outToServer.writeBytes(s);
+				//ystem.out.println("Sending");
+			}
+			br.close();
+			client.close();
+		  }catch(Exception ex)
+		  
+		  {
+			  System.out.println("Error"+ex);
+		  }
+      
+	  }
+    });
+	
+    }
+}
---
+++ ServerApplication.java
@@ -0,0 +1,148 @@
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.*; 
+import java.lang.*;
+import java.util.*;
+import java.nio.file.*;
+import javax.swing.Timer;
+class ServerApplication {
+	static String[] alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8",
+        "9", "0","!", ",", "?", ".", "'"," ","-","@"};
+	static String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+        "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+        "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+        "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
+        "-....", "--...", "---..", "----.", "-----","-.-.--", "--..--", "..--..", ".-.-.-", ".----."," ","_...._",".__._."};
+
+ public static void main(String args[]) throws Exception {
+	 
+  String clientSentence;
+  String capitalizedSentence;
+  ServerSocket welcomeSocket = new ServerSocket(6666);
+
+  while (true) {
+   Socket connectionSocket = welcomeSocket.accept();
+   BufferedReader br =new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+   clientSentence = br.readLine();
+  //System.out.println("Received: " + clientSentence);
+  
+	
+	//String s;
+	
+	String build = "";
+    String change = clientSentence.trim();
+    String[] words = change.split("   ");
+    for (String word : words) {
+        for(String letter : word.split(" ")){
+            for(int x=0;x<morse.length;x++){
+                if(letter.equals(morse[x]))
+                    build=build+alpha[x];
+            }
+        }
+        build+=" ";
+    }
+	//System.out.println(build);
+	FileWriter fw = new FileWriter("output.txt", false);
+	//while ((s = br.readLine()) != null) { 
+				fw.write(clientSentence); 
+				fw.flush();
+				//outToServer.writeBytes(s);
+			//}
+			br.close();
+			fw.close();
+			
+			JPanel panel = new JPanel(); 
+			JFrame frame = new JFrame("ApMSaM");
+			frame.setSize(400, 100);
+			JLabel label = new JLabel("Password");
+			JTextField tf = new JTextField(4); 
+			JButton send = new JButton("OK");
+			panel.add(tf);
+			panel.add(send);
+			frame.getContentPane().add(BorderLayout.SOUTH, panel);
+            frame.setVisible(true);
+			
+			
+			send.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+      {try
+	  {
+		  BufferedWriter bw=new BufferedWriter(new FileWriter("Passwd.txt"));
+		bw.write(tf.getText());
+		bw.close(); 
+	  }
+	  catch(Exception ex)
+		  
+		  {
+			  System.out.println("Error"+ex);
+		  }
+		 frame.dispose(); 
+	  }	
+			});
+			
+			Thread.sleep(5000);
+			
+			BufferedReader reader = new BufferedReader(new FileReader("Passwd.txt"));
+			String pass=reader.readLine();
+			reader.close();
+			//System.out.println(pass);
+			//int p=Integer.parseInt(pass);
+			
+			String compass=build.substring(0,4);
+			String timer1=build.substring(5,10);
+			String timer12=build.substring(5,7);
+			String build1=build.substring(11);
+			//System.out.println(timer);
+			int c=Integer.parseInt(timer1);
+			
+			if(pass.equals(compass))
+			{
+			//JFrame frame1= new JFrame("Confidential");
+			//JOptionPane.showMessageDialog(frame1,build+"\n You Got "+c+" Seconds","Confidential",JOptionPane.INFORMATION_MESSAGE);
+			//Thread.sleep(c);
+			//frame1.dispose();
+              
+            final JOptionPane optionPane = new JOptionPane(build1+"\n You Got "+timer12+" Seconds", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
+
+			final JDialog dialog = new JDialog();
+			dialog.setTitle("Message");
+			dialog.setModal(true);
+
+			dialog.setContentPane(optionPane);
+
+			dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+			dialog.pack();
+
+			//create timer to dispose of dialog after 5 seconds
+			Timer timer = new Timer(c, new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+			dialog.dispose();
+    }
+});
+timer.setRepeats(false);//the timer should only go off once
+
+//start timer to close JDialog as dialog modal we must start the timer before its visible
+timer.start();
+
+dialog.setVisible(true);
+           			 
+			Files.delete(Paths.get("passwd.txt"));
+			Files.delete(Paths.get("output.txt"));
+			}
+			else 
+			{
+				System.out.println("All DATA DELETED");
+				Files.delete(Paths.get("output.txt"));
+				Files.delete(Paths.get("passwd.txt"));
+				
+			}
+  }
+ }
+}
